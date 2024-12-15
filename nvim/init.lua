c8bc83e727edd7ed5bc63524c2dbd2394ed2f0ba
lesacar <69954403+lesacar@users.noreply.github.com>
2024-12-15 04:00:54 +0100

    .

--- nvim/init.lua
+++ nvim/init.lua
@@ -611,6 +611,8 @@ require("lazy").setup({
 				-- But for many setups, the LSP (`ts_ls`) will work just fine
 				-- ts_ls = {},
 				--
+                ts_ls = {
+                },
 
 				lua_ls = {
 					-- cmd = {...},
